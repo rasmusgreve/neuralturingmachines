799e6aa3ccfe078fa5e192e67c7809189a39b97b
Rasmus Boll Greve <ragr@schantz.com>
2015-06-19 13:05:14 +0200

    Added our classpath to ANJI classpath

--- anji_2_01/evolve.bat
+++ anji_2_01/evolve.bat
@@ -1,4 +1,4 @@
 @echo off
-set MYCLASSPATH=.\lib\anji.jar;.\lib\jgap.jar;.\lib\log4j.jar;.\lib\jakarta-regexp-1.3.jar;.\lib\clibwrapper_jiio.jar;.\lib\mlibwrapper_jiio.jar;.\lib\jai_imageio.jar;.\lib\hb16.jar;.\lib\jcommon.jar;.\lib\jfreechart.jar;.\lib\jakarta-regexp-1.3.jar;.\properties
+set MYCLASSPATH=.\lib\anji.jar;.\lib\jgap.jar;.\lib\log4j.jar;.\lib\jakarta-regexp-1.3.jar;.\lib\clibwrapper_jiio.jar;.\lib\mlibwrapper_jiio.jar;.\lib\jai_imageio.jar;.\lib\hb16.jar;.\lib\jcommon.jar;.\lib\jfreechart.jar;.\lib\jakarta-regexp-1.3.jar;.\properties;.\..\bin
 java -classpath %MYCLASSPATH% -Xms256m -Xmx384m com.anji.neat.Evolver %1
 
