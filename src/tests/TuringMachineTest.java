60a2b43b5110b469e2393afd3cace9d9a2f77b91
Rasmus Boll Greve <ragr@schantz.com>
2015-06-22 15:05:04 +0200

    Started implementing unit tests of turing machine

--- src/tests/TuringMachineTest.java
+++ src/tests/TuringMachineTest.java
@@ -2,6 +2,26 @@ package tests;
 
 import org.junit.*;
 
-public class TuringMachineTest  {
+import turing.TuringMachine;
+import turing.TuringMachine.HeadVariables;
 
+public class TuringMachineTest {
+
+	@Test
+	public void simpleTest(){
+		TuringMachine machine = new TuringMachine(100, 1, 1, 1, null);
+		
+		HeadVariables hv = new HeadVariables();
+		
+		hv.addWrite(new double[]{0}, new double[]{1}, new double[]{0}, 0, 0, new double[]{0}, 4);
+		hv.addRead(new double[]{1}, 1, 0, new double[]{0}, 4);
+		
+		double[][] result = machine.processInput(hv);
+		
+		for (int i = 0; i < result.length; i++)
+			System.out.println(i + " -> " + result[i][0]);
+		
+		Assert.assertTrue("Test", true);
+	}
+	
 }
