23b588073b739f248efa2b2c9618c4833fbba715
Rasmus Boll Greve <ragr@schantz.com>
2015-07-29 15:13:21 +0200

    Implemented content jump test for minimal turing machine

--- src/dk/itu/ejuuragr/tests/MinimalTuringMachineTest.java
+++ src/dk/itu/ejuuragr/tests/MinimalTuringMachineTest.java
@@ -1,5 +1,9 @@
 package dk.itu.ejuuragr.tests;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import com.anji.util.Properties;
 
 import dk.itu.ejuuragr.fitness.Utilities;
@@ -7,42 +11,85 @@ import dk.itu.ejuuragr.turing.MinimalTuringMachine;
 
 public class MinimalTuringMachineTest {
 
-	public static void main(String[] args) {
+	MinimalTuringMachine tm;
+	
+	@Before
+	public void before(){
 		Properties props = new Properties();
 		props.setProperty("tm.m", "2");
 		props.setProperty("tm.shift.length", "3");
-		MinimalTuringMachine tm = new MinimalTuringMachine(props);
-		
-		//Inputs to tm:
+		tm = new MinimalTuringMachine(props);
+	}
+	
+	//Inputs to tm:
 		//m * write
 		//1 * interpolation
+		//1 * content jump
 		//(3) * shift
-		
+	
+	//write
+		//jump
+		//shift
+		//read
+	
+	@Test
+	public void testShift1(){
 		double[] tmstim = new double[]{
 				0, 1,	//Write
 				1,		//Write interpolation
-				0, 0, 1	//Shift
+				0,		//Content jump
+				1, 0, 0	//Shift
 		};
 		
 		double[] result = tm.processInput(tmstim)[0];
-		System.out.println(Utilities.toString(result));
-		
+		Assert.assertArrayEquals(new double[]{0, 0}, result, 0.0);
 		
 		tmstim = new double[]{
 				0, 0,	//Write
 				0,		//Write interpolation
+				0,		//Content jump
 				0, 0, 1	//Shift
 		};
 		
 		result = tm.processInput(tmstim)[0];
-		
-		System.out.println(Utilities.toString(result));
-		
+
+		Assert.assertArrayEquals(new double[]{0, 1}, result, 0.0);
 	}
 	
-	//write
-	//jump
-	//shift
-	//read
+	@Test
+	public void testContentBasedJump1(){
+		//Write jump target
+		double[] tmstim = new double[]{
+				0, 1,	//Write
+				1,		//Write interpolation
+				0,		//Content jump
+				0, 0, 1	//Shift
+		};
+		double[] result = tm.processInput(tmstim)[0];
+		
+		//Write jump result
+		tmstim = new double[]{
+				1, 0,	//Write
+				1,		//Write interpolation
+				0,		//Content jump
+				0, 0, 1	//Shift
+		};
+		result = tm.processInput(tmstim)[0];
+		
+		//Jump, shift, and read
+		tmstim = new double[]{
+				0, 1,	//Write
+				0,		//Write interpolation
+				1,		//Content jump
+				0, 0, 1	//Shift
+		};
+		result = tm.processInput(tmstim)[0];
+		System.out.println(Utilities.toString(result));
+		Assert.assertArrayEquals(new double[]{1, 0}, result, 0.0);
+	}
+	
+	
+	
+	
 	
 }
