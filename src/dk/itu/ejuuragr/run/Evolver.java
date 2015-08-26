6bbc6ec88dd5e4a03ab00e7a57fe3a81c503d865
Rasmus Boll Greve <ragr@schantz.com>
2015-08-26 09:45:01 +0200

    Added seed chooser to evolver

--- src/dk/itu/ejuuragr/run/Evolver.java
+++ src/dk/itu/ejuuragr/run/Evolver.java
@@ -1,5 +1,7 @@
 package dk.itu.ejuuragr.run;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 import com.anji.util.Properties;
@@ -13,6 +15,17 @@ public class Evolver {
 
 	public static void main(String[] args) throws Throwable {
 		Properties props = new Properties(args[0]);
+		if (!props.containsKey("random.seed")){
+			System.out.println("Properties contain no random seed!");
+			System.out.println("Enter desired random seed:");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String seed = br.readLine();
+			props.setProperty("random.seed", seed);
+			props.setProperty("log4j.appender.A1.File", "./db/log" + seed + ".txt");
+			props.setProperty("persistence.base.dir", "./db"+seed);
+		}
+		
+		
 		com.anji.neat.Evolver evolver = new com.anji.neat.Evolver();
 		
 		TuringController controller = FitnessEvaluator.loadController(props);
