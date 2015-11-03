92363605f1cfb2214405010eed1a1064e666eaa7
Emil Juul Jacobsen <emil.juul.jacobsen@gmail.com>
2015-11-03 10:15:27 +0100

    hidden locations

--- src/dk/itu/ejuuragr/fitness/HyperTMazeEvaluator.java
+++ src/dk/itu/ejuuragr/fitness/HyperTMazeEvaluator.java
@@ -2,18 +2,22 @@ package dk.itu.ejuuragr.fitness;
 
 import java.util.Iterator;
 import java.util.Map.Entry;
+import java.util.SortedSet;
 
+import org.jgapcustomised.Allele;
 import org.jgapcustomised.Chromosome;
+import org.jgapcustomised.ChromosomeMaterial;
 
 import com.anji_ahni.integration.Activator;
+import com.anji_ahni.neat.ConnectionAllele;
+import com.anji_ahni.neat.NeuronAllele;
+import com.anji_ahni.neat.NeuronType;
 import com.ojcoleman.ahni.evaluation.BulkFitnessFunctionMT;
 import com.ojcoleman.ahni.hyperneat.Properties;
 import com.ojcoleman.ahni.util.Point;
 
 import dk.itu.ejuuragr.domain.Simulator;
-import dk.itu.ejuuragr.domain.tmaze.RoundsTMaze;
 import dk.itu.ejuuragr.fitness.Utilities.ActivatorProxy;
-import dk.itu.ejuuragr.turing.TuringController;
 
 public class HyperTMazeEvaluator extends BulkFitnessFunctionMT {
 
@@ -23,6 +27,7 @@ public class HyperTMazeEvaluator extends BulkFitnessFunctionMT {
 	private Properties properties;
 	private com.anji.util.Properties anjiProps;
 	private int m;
+	private int hidden;
 	
 	
 	private com.anji.util.Properties convertProps(Properties props){
@@ -43,6 +48,7 @@ public class HyperTMazeEvaluator extends BulkFitnessFunctionMT {
 		this.anjiProps = convertProps(props);
 		
 		this.m = props.getIntProperty("tm.m");
+		this.hidden = props.getIntProperty("initial.topology.num.hidden.neurons");
 	}
 
 	@Override
@@ -74,14 +80,25 @@ public class HyperTMazeEvaluator extends BulkFitnessFunctionMT {
 		return fitness;
 	}
 	
+	private NeuronAllele findWithID(SortedSet<Allele> alleles, Long srcNeuronId) {
+		for(Allele a : alleles) {
+			if(a.getInnovationId() == srcNeuronId) {
+				return (NeuronAllele)a;
+			}
+		}
+		return null;
+	}
+
+
 	@Override
 	public int[] getLayerDimensions(int layer, int totalLayerCount) {
-		//TODO: HARDCODED M=2
 		if (layer == 0){ // Input layer.
 			// 3 range sensors plus reward plus TM 1+2 (or whatever m is).
 			return new int[] { 3+1+this.m, 1 };
+		} else if (this.hidden > 0 && layer == 1) {
+			return new int[] { this.hidden, 1};
 		} else if (layer == totalLayerCount - 1) { // Output layer.
-			return new int[] { 1+this.m+5, 1 }; // 1 Domain (S), 2 TM data, 5 TM control (W,J,L,S,R)
+			return new int[] { 1+this.m+5, 1}; // 1 Domain (S), 2 TM data, 5 TM control (W,J,L,S,R)
 		}
 		return null;
 	}
@@ -91,7 +108,7 @@ public class HyperTMazeEvaluator extends BulkFitnessFunctionMT {
 		// Coordinates are given in unit ranges and translated to whatever range is specified by the
 		// experiment properties.
 		Point[] positions = null;
-		double space = 1.0 / (this.m-1);
+		
 		if (layer == 0) { // Input layer.
 			positions = new Point[4 + this.m];
 			positions[0] = new Point(0,   0, 0);
@@ -99,16 +116,26 @@ public class HyperTMazeEvaluator extends BulkFitnessFunctionMT {
 			positions[2] = new Point(1,   0, 0);
 			positions[3] = new Point(0.5, 0.4, 0);
 			
-			
+			double space = 1.0 / (this.m-1);
 			for(int i = 0; i < this.m; i++) {
-				positions[4+i] = new Point(space*i, 0.25, 0);
+				positions[4+i] = new Point(this.m == 1 ? 0.5 : space*i, 0.25, 0);
 			}
 
-		} else if (layer == totalLayerCount - 1) { // Output layer.
+		} else if (this.hidden > 0 && layer == 1) {
+			positions = new Point[this.hidden];
+			
+			double space = 1.0 / (this.hidden-1);
+			for(int i = 0; i < this.hidden; i++) {
+				positions[i] = new Point(this.hidden == 1 ? 0.5 : space*i, 0.5, 0);
+			}
+			
+		}else if (layer == totalLayerCount - 1) { // Output layer.
 			positions = new Point[1+5+this.m];
 			positions[0] = new Point(0.5,  0.6, 0);
+			
+			double space = 1.0 / (this.m-1);
 			for(int i = 0; i < this.m; i++) {
-				positions[1+i] = new Point(space*i, 0.75, 0);
+				positions[1+i] = new Point(this.m == 1 ? 0.5 : space*i, 0.75, 0);
 			}
 			positions[1+this.m] = new Point(0,    1, 0);
 			positions[1+this.m+1] = new Point(1,    1, 0);
