4e3942987ae94de197b1e369beb6b02e98195645
Rasmus Boll Greve <ragr@schantz.com>
2015-11-30 12:33:31 +0100

    Cleanup, removed images

--- hyper-run.bat
+++
@@ -1,4 +0,0 @@
-cd bin/
-java -Xmx3G -Xms1G -cp ".;../lib/*;../db" com/ojcoleman/ahni/hyperneat/Run ../tmaze-turingmachine.properties
-cd ..
-pause
---
+++ structured_run_bats/hyper-run.bat
@@ -0,0 +1,4 @@
+cd bin/
+java -Xmx4G -Xms2G -cp ".;../lib/*;../db" com/ojcoleman/ahni/hyperneat/Run ../tmaze-turingmachine.properties
+cd ..
+pause
